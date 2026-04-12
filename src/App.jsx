6506a04c55ed7ebc377be068899cd35e8cb0be82
Doniamagdy <donallino@gmail.com>
2026-04-12 16:18:07 +0200

    setup supabase + env example

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import { supabase } from "./supabaseClient.js";
+
 
 function App() {
+  const [guestName, setIGuestName] = useState([]);
+
+// console.log(supabase.supabaseUrl);
+
+
+  useEffect(() => {
+    getGuestName();
+  }, []);
+
+  async function getGuestName() {
+    const { data, error } = await supabase.from("guests").select();
+    console.log(data);
+
+    setIGuestName(data);
+  }
 
   return (
-    
-    <h1 className='text-center'>Hotel</h1>
-  
-  )
+    <div>
+      {guestName.map((guest) => (
+        <p key={guest.id}>{guest.guest_full_name}</p>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
